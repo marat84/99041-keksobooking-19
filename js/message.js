9ac314e356ee423c374909a5a5515cf2d5b36ba8
Marat <marat84ay@gmail.com>
2020-03-11 12:05:17 +0500

    Fix: error message view

--- js/message.js
+++ js/message.js
@@ -5,16 +5,40 @@
   var titleErrorMessage = templateError.querySelector('.error__message');
   var buttonErrorMessage = templateError.querySelector('.error__button');
 
-  var buttonErrorClickHandler = function () {
+  var closeMessage = function () {
+    buttonErrorMessage.removeEventListener('click', buttonErrorClickHandler);
+    templateError.removeEventListener('click', templateErrorClickHandler);
+    document.removeEventListener('keydown', documentKeyDownHandler);
+
     templateError.remove();
+
     window.start.deActivatedPage();
+  };
+
+  var templateErrorClickHandler = function (evt) {
+    var target = evt.target;
+    if (target && !target.matches('.error__button')) {
+      closeMessage();
+    }
+  };
+
+  var documentKeyDownHandler = function (evt) {
+    if (evt.key === window.utils.keyEscape) {
+      closeMessage();
+    }
+  };
+
+  var buttonErrorClickHandler = function () {
+    closeMessage();
   };
 
   var showMessage = function (message) {
 
-    titleErrorMessage.textContent = message.title;
+    titleErrorMessage.textContent = message;
 
     buttonErrorMessage.addEventListener('click', buttonErrorClickHandler);
+    templateError.addEventListener('click', templateErrorClickHandler);
+    document.addEventListener('keydown', documentKeyDownHandler);
 
     document.body.appendChild(templateError);
   };
