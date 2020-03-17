a3ed6a4c4f5d3f45c60549a5910872d8e9fbb2d6
Marat <marat84ay@gmail.com>
2020-03-17 12:04:48 +0500

    Fix: use enum

--- js/utils.js
+++ js/utils.js
@@ -9,11 +9,14 @@
   var MAIN_PIN_HEIGHT = 62;
   var SMALL_PIN_HEIGHT = 70;
   var SMALL_PIN_WIDTH = 50;
-  var MAP_WIDTH = 1200;
-  var MAP_MIN_HEIGHT = 130;
-  var MAP_MAX_HEIGHT = 630;
   var HALF_PIN_WIDTH = MAIN_PIN_WIDTH / 2;
   var PIN_HEIGHT_WITH_TAIL = MAIN_PIN_HEIGHT + MAIN_PIN_TAIL_HEIGHT;
+  var TIME_DEBOUNCE = 500;
+  var Map = {
+    WIDTH: 1200,
+    MIN_HEIGHT: 130,
+    MAX_HEIGHT: 630
+  };
   var TIMEOUT_DEBOUNCE = 500;
 
   var debounce = function (cb) {
@@ -32,18 +35,6 @@
     return createDebounce;
   };
 
-  var getRandomValue = function (values) {
-    return values[Math.floor(Math.random() * values.length)];
-  };
-
-  var getRandomNumber = function (min, max) {
-    return Math.round(Math.random() * (max - min)) + min;
-  };
-
-  var getRandomSlicedArray = function (array) {
-    return array.slice(0, getRandomNumber(1, array.length));
-  };
-
   var getPinPosition = function (pin) {
     return {
       x: parseInt(pin.style.left, 10),
@@ -60,14 +51,9 @@
     mainPinTailHeight: MAIN_PIN_TAIL_HEIGHT,
     mainPinWidth: MAIN_PIN_WIDTH,
     mainPinHeight: MAIN_PIN_HEIGHT,
-    mapWidth: MAP_WIDTH,
-    mapMinHeight: MAP_MIN_HEIGHT,
-    mapMaxHeight: MAP_MAX_HEIGHT,
     halfPinWidth: HALF_PIN_WIDTH,
     pinHeightWithTail: PIN_HEIGHT_WITH_TAIL,
-    getRandomValue: getRandomValue,
-    getRandomNumber: getRandomNumber,
-    getRandomSlicedArray: getRandomSlicedArray,
+    Map: Map,
     getPinPosition: getPinPosition,
     debounce: debounce
   };
